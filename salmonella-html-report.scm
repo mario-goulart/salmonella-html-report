e05faad67459739d7da620662c9216e6e457adef
Mario Domenech Goulart <mario@parenteses.org>
2024-12-11 20:06:45 +0100

    Use "<!DOCTYPE html>" as doctype

--- salmonella-html-report.scm
+++ salmonella-html-report.scm
@@ -138,10 +138,8 @@
                                  ,label))))))
             locs))))
 
-(define (page-template content #!key title)
-  `((literal
-     "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\""
-     "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\">")
+(define (page-template content #!key title (use-css? #t))
+  `((literal "<!DOCTYPE html>")
     (html (@ (xmlns "http://www.w3.org/1999/xhtml"))
           (head
            (meta (@ (charset "utf-8")))
